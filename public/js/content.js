67388480dd1e2876c7d088bc0e85e5d644f9e956
Joel Smith <jsumnersmith@gmail.com>
2015-01-22 16:26:31 -0500

    Use data attributes like a normal person

--- public/js/content.js
+++ public/js/content.js
@@ -1,10 +1,10 @@
 apos.widgetPlayers.toc = function($el) {
 
   var $area =  $el.closest('.apos-content');
-  $el.toc({
+  $el.find('[data-toc]').toc({
     'container': $area,
-    'selectors': 'h3,h4,h5,h6'
-
+    'selectors': 'h3,h4,h5,h6',
+    'smoothScrolling': 'false'
   });
 
 }
